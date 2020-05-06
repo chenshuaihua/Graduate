937d74682bccd15750f887ae0f43c19bd539bb07
chenshuaihua <15811282778@163.com>
2020-05-06 19:49:11 +0800

    使用SVM完成了chb01和chb03的多属性分类

---
+++ 程序/CHB-MIT数据集/DT/标准差/chb01_biaozhuncha_multi_attri.m
@@ -0,0 +1,21 @@
+clc
+clear
+
+%%%%%读取数据
+x1 = load("E:\大学\大四学年（2019-2020）\下学期\毕设\数据\CHB-MIT数据集\标准差\chb01_nor_biaozhuncha.txt");
+y1 = load("E:\大学\大四学年（2019-2020）\下学期\毕设\数据\CHB-MIT数据集\标准差\chb01_epil_biaozhuncha.txt");
+
+x2 = load("E:\大学\大四学年（2019-2020）\下学期\毕设\数据\CHB-MIT数据集\标准差\chb01_nor_biaozhuncha_d1.txt");
+y2 = load("E:\大学\大四学年（2019-2020）\下学期\毕设\数据\CHB-MIT数据集\标准差\chb01_epil_biaozhuncha_d1.txt");
+
+x3 = load("E:\大学\大四学年（2019-2020）\下学期\毕设\数据\CHB-MIT数据集\标准差\chb01_nor_biaozhuncha_d2.txt");
+y3 = load("E:\大学\大四学年（2019-2020）\下学期\毕设\数据\CHB-MIT数据集\标准差\chb01_epil_biaozhuncha_d2.txt");
+
+x4 = load("E:\大学\大四学年（2019-2020）\下学期\毕设\数据\CHB-MIT数据集\标准差\chb01_nor_biaozhuncha_d3.txt");
+y4 = load("E:\大学\大四学年（2019-2020）\下学期\毕设\数据\CHB-MIT数据集\标准差\chb01_epil_biaozhuncha_d3.txt");
+
+x5 = load("E:\大学\大四学年（2019-2020）\下学期\毕设\数据\CHB-MIT数据集\标准差\chb01_nor_biaozhuncha_d4.txt");
+y5 = load("E:\大学\大四学年（2019-2020）\下学期\毕设\数据\CHB-MIT数据集\标准差\chb01_epil_biaozhuncha_d4.txt");
+
+x6 = load("E:\大学\大四学年（2019-2020）\下学期\毕设\数据\CHB-MIT数据集\标准差\chb01_nor_biaozhuncha_d5.txt");
+y6 = load("E:\大学\大四学年（2019-2020）\下学期\毕设\数据\CHB-MIT数据集\标准差\chb01_epil_biaozhuncha_d5.txt");
